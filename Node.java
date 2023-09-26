400b2930e3192ca4aa3d95b1ea014db79efbb9a0
Aljoharah Aljubair <116559876+Aljoharah-Aljubair@users.noreply.github.com>
2023-09-26 23:47:00 +0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -2,6 +2,7 @@ public class Node{
     public static void main(String[] args){
         int x =0;
         int y = x+1;
-        System.out.print(y);
+        System.out.print(y+x);
+        
     }
 }
